e885170d431be2837131ea4921217ae9e6732ada
Ivo <ivol@Ivos-MacBook-Air.local>
2017-12-21 20:17:50 -0600

    refined commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ var losses = 0;
 var computerNumber;
 var counter;
 
-
 function startGame() {
 
 var counter = 0;
@@ -12,10 +11,11 @@ $(".your-total-score").text(counter);
 computerNumber = Math.floor(Math.random() * 101) + 19;
 $(".number-to-match").text(computerNumber);
 
-
 $(".crystal-image").each(function() {
 $(this).attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1)
+
 });
+
 }
 startGame();
 
@@ -23,23 +23,18 @@ var counter = 0;
 
 crystals = ["assets/images/yellow.png", "assets/images/blue.png", "assets/images/green.png", "assets/images/purple.png"]
 
-
 for (var i = 0; i < crystals.length; i++) {
 
     var imageCrystal = $("<img>");
     
-
 imageCrystal.addClass("crystal-image");
 
-
 imageCrystal.attr("src", crystals[i]);
-
 
 imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
 
 $("#crystals").append(imageCrystal);
 }
-
 
 $(".crystal-image").on("click", function() {
 
@@ -51,28 +46,22 @@ console.log(crystalValue);
 
 $(".your-total-score").text(counter);
 
-
 if (counter === computerNumber) {
     wins++
     $("#wins").text(wins);
     $("#status").text("You rock!");
-    // alert("You win!");
     counter = 0;
-    
     crystalValue = 0;
-    startGame();
-    
+    startGame(); 
 }
 
 else if (counter >= computerNumber) {
     losses++
     $("#losses").text(losses);
     $("#status").text("You suck!");
-    
     counter = 0;
     crystalValue = 0;
-    startGame();
-    
+    startGame();  
 }
 
 });
